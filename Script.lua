103a3c71eaecb4a4b0dd3b861c6e04a85fdcebf2
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-07-17 13:02:05 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,5 @@
+--Script By HKTD, TikTok: https://www.tiktok.com/@hktd_roblox
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
